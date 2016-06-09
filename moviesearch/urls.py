977c611361fab57bf510afe5f7dde4b1c00a42e9
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 14:50:26 +0530

    Make second query optional

--- moviesearch/urls.py
+++ moviesearch/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'([\w ]+)/(?P<no_of_queries>[\d]+)?/$', views.get_movie_results),
+    url(r'([\w ]+)/([\d]+)$', views.get_movie_results),
+    url(r'([\w ]+)$', views.get_movie_results),
     url(r'^$', views.index, name='index'),
 ]
