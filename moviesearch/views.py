ed85098bd64fd4348c0977122ab29adffc1bc952
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 12:16:54 +0530

    Optional query attribute

--- moviesearch/views.py
+++ moviesearch/views.py
@@ -6,9 +6,9 @@ def index(request):
     return HttpResponse("Welcome to UMDb!")
 
 
-def get_movie_results(request, moviename, no_of_queries=1):
+def get_movie_results(request, moviename):
     #moviename = request.GET['t']
     #no_of_queries = request.GET['q']
     #res = search.get_all_movies(moviename, no_of_queries)
-    return HttpResponse(no_of_queries)
+    return HttpResponse(moviename)
     #return JsonResponse(res, safe=False)
