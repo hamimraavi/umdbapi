85b6fb8cd111d9b38a4e49f36004be800c95ab1c
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 11:40:10 +0530

    Optional query attribute

--- moviesearch/urls.py
+++ moviesearch/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'.*$', views.results),
+    url(r'([\w+ ]+)(?:/[0-9]+)$', views.get_movie_results),
     url(r'^$', views.index, name='index'),
 ]
--- moviesearch/views.py
+++ moviesearch/views.py
@@ -1,14 +1,13 @@
 import search
-from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("Welcome to UMDb!")
 
 
-def results(request):
-    moviename = request.GET['t']
-    no_of_queries = request.GET['q']
+def get_movie_results(request, moviename, no_of_queries=1):
+    #moviename = request.GET['t']
+    #no_of_queries = request.GET['q']
     res = search.get_all_movies(moviename, no_of_queries)
     return JsonResponse(res, safe=False)
