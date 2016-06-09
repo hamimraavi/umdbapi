42e941a72975fac835b9185dd26d77062c64e2ce
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 13:11:38 +0530

    Test URL attributes

--- moviesearch/urls.py
+++ moviesearch/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'.*$', views.get_movie_results),
+    url(r'(\w+)/(\d+)$', views.get_movie_results),
     url(r'^$', views.index, name='index'),
 ]
--- moviesearch/views.py
+++ moviesearch/views.py
@@ -6,8 +6,8 @@ def index(request):
     return HttpResponse("Welcome to UMDb!")
 
 
-def get_movie_results(request):
-    moviename = request.GET['t']
-    no_of_queries = request.GET['q']
+def get_movie_results(request, moviename, no_of_queries):
+    #moviename = request.GET['t']
+    #no_of_queries = request.GET['q']
     res = search.get_all_movies(moviename, no_of_queries)
     return JsonResponse(res, safe=False)
