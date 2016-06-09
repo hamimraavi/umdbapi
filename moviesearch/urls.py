7036e2cb5319ecc9f9eb4372a90ad0ccc6668dc0
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 15:37:46 +0530

    Handle incorrect queries

--- moviesearch/urls.py
+++ moviesearch/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^([\w ]+)/$', views.get_movie_results),
     url(r'^([\w ]+)/([\d]+)/$', views.get_movie_results),
-    url(r'^([\w ]+)/(.*)$', views.get_movie_results),
-    url(r'^$', views.index, name='index'),
+    url(r'^([\w ]+)/(.*)$', views.index),
+    url(r'^$', views.index),
 ]
