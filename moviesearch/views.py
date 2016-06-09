6d920c285f7342782b266728c6fbea8bcf9683ad
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-09 11:51:43 +0530

    Optional query attribute

--- moviesearch/views.py
+++ moviesearch/views.py
@@ -10,5 +10,5 @@ def get_movie_results(request, moviename, no_of_queries=1):
     #moviename = request.GET['t']
     #no_of_queries = request.GET['q']
     #res = search.get_all_movies(moviename, no_of_queries)
-    return HttpResponse(str(request+"\n"+moviename+"\n"+no_of_queries))
+    return HttpResponse(no_of_queries)
     #return JsonResponse(res, safe=False)
